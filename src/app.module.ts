623ce2700204e3cb30a600edf7b8d417e7f6e04f
hyewon4052 <s2311@e-mirim.hs.kr>
2025-04-29 20:08:35 +0900

    database 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+// src/app.module.ts
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from '../config/database/typeorm.config';
 import { AppService } from './app.service';
+import { PostsModule } from './res/posts/posts.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: typeOrmConfig,
+    }),
+    PostsModule,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { DataSource } from 'typeorm';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
+  constructor(private readonly dataSource: DataSource) {}
+
+  async onModuleInit() {
+    try {
+      if (!this.dataSource.isInitialized) {
+        await this.dataSource.initialize();
+      }
+      console.log('db 연결됨');
+    } catch (error) {
+      console.error('db 연결 실패', error);
+    }
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
