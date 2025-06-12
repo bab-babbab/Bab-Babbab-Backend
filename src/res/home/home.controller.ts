65af8dd23dce2b3b441e1d404a8a84b7fbdda969
gheun <gheun098@gmail.com>
2025-06-12 09:37:57 +0900

    home에서 user 정보 호출

--- src/res/home/home.controller.ts
+++ src/res/home/home.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { HomeService } from './home.service';
+import { UserInfoDto } from '../user/dto/user-info.dto';
 
 @Controller('home')
 export class HomeController {
   constructor(private readonly homeService: HomeService) {}
+
+  @Get('user/:id')
+  async getUserInfo(@Param('id') userId: string): Promise<UserInfoDto> {
+    return this.homeService.getUserInfo(userId);
+  }
 }
--- src/res/home/home.service.ts
+++ src/res/home/home.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../user/entities/user.entity';
+import { UserInfoDto } from '../user/dto/user-info.dto';
 
 @Injectable()
-export class HomeService {}
+export class HomeService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async getUserInfo(userId: string): Promise<UserInfoDto> {
+    const user = await this.userRepository.findOne({
+      where: { id: userId },
+      select: ['id', 'name', 'profile', 'message'],
+    });
+
+    if (!user) {
+      throw new Error('유저를 찾을 수 없습니다.');
+    }
+
+    // 매핑
+    return {
+      id: user.id,
+      name: user.name,
+      profile: user.profile,
+      message: user.message,
+    };
+  }
+
+}
